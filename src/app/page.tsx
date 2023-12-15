ebd8e67f288ed3ad33dd3f54090178fd795c37d7
Felipe <flpc.dev@gmail.com>
2023-12-15 18:59:44 -0300

    feat: finish homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import { Budget } from "@/components/Budget";
 import { Contact } from "@/components/Contact";
 import { Design } from "@/components/Design";
+import { Gallery } from "@/components/Gallery";
 
 export default function Home() {
   return (
     <>
       <Contact />
       <Design />
+      <Gallery />
+      <Budget />
     </>
   )
 }
