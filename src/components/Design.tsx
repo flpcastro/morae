937e5cb4f363b8517dc976748ef34794b972feb4
Felipe <flpc.dev@gmail.com>
2023-12-15 19:00:33 -0300

    feat: finish Design component

--- src/components/Design.tsx
+++ src/components/Design.tsx
@@ -4,25 +4,25 @@ import Link from 'next/link'
 
 export function Design() {
   return (
-    <div className="px-80">
+    <div className="px-80 flex items-center gap-28 mb-20">
       <Image 
         src={livingRoomImg}
         alt="Imagem de uma sala"
         className="w-[537px] h-[507px] shadow-negative mt-[180px]"
       />
 
-      <div>
-        <div>
-          <span>
+      <div className='flex flex-col gap-6'>
+        <div className='max-w-xl bg-dark100 p-7'>
+          <span className='font-semibold text-white text-2xl'>
             Design Minimalista
           </span>
 
-          <p>
+          <p className='font-medium text-white text-base leading-6 mt-5'>
             Móveis apenas essenciais e com design moderno, além de revestimentos bonitos e em cores neutras para manter a casa sempre com um estilo clean e suave.
           </p>
         </div>
 
-        <Link href="#">
+        <Link href="#" className='font-medium text-lg text-dark100 underline'>
           Quero saber mais sobre os designs
         </Link>
       </div>
